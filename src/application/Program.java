7794a9da8966f8a0d9c1ae048586271674356b4e
natolara <larasantos22@hotmail.com>
2021-08-23 17:18:27 -0300

    findById implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,10 +12,15 @@ public class Program {
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		//System.out.println(obj);
 
 		Seller sl = new Seller(21, "Bob", "Bob@gmail", new Date(), 3000.0, obj);
-		System.out.println(sl);
+		//System.out.println(sl);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+		
 	}
 
 }
