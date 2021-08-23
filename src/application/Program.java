981c5e46d6021843ab833a24b005f542020514af
natolara <larasantos22@hotmail.com>
2021-08-23 17:27:41 -0300

    reuso intaciacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,15 +11,12 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		//System.out.println(obj);
-
-		Seller sl = new Seller(21, "Bob", "Bob@gmail", new Date(), 3000.0, obj);
-		//System.out.println(sl);
 		
+		System.out.println("====Teste  1 seller findById==========");		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
 		
 	}
 
