30cefff8678ac8ff314634191d7aeca8b8a753e3
natolara <larasantos22@hotmail.com>
2021-08-23 15:57:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
+
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
+		Seller sl = new Seller(21, "Bob", "Bob@gmail", new Date(), 3000.0, obj);
+		System.out.println(sl);
 	}
 
 }
