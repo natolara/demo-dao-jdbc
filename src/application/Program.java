e1c7c51f83d00947bf0e48fd8fbf672575b76b59
natolara <larasantos22@hotmail.com>
2021-08-24 08:35:01 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,33 +12,38 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		
-		System.out.println("====Teste  1 seller findById==========");		
+		System.out.println("====Teste  1 seller findById==========");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
-		System.out.println("\n====Teste  2 seller findByDepartment==========");	
-		Department department = new Department(2,null);
+
+		System.out.println("\n====Teste  2 seller findByDepartment==========");
+		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
+
 		for (Seller seller2 : list) {
 			System.out.println(seller2);
 		}
-		
-		System.out.println("\n====Teste  3 seller findAll==========");	
-		
+
+		System.out.println("\n====Teste  3 seller findAll==========");
+
 		list = sellerDao.findAll();
-		
+
 		for (Seller seller2 : list) {
 			System.out.println(seller2);
 		}
-		
-System.out.println("\n====Teste  4 seller insert==========");	
-		
+
+		System.out.println("\n====Teste  4 seller insert==========");
+
 		Seller newSeller = new Seller(null, "Greg", "Gre@mail.docm", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! new id = "+ newSeller.getId());
-	}
+		System.out.println("Inserted! new id = " + newSeller.getId());
 
+		System.out.println("\n====Teste  5 seller update==========");
+
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waina");
+		sellerDao.update(seller);
+		System.out.println("uodate completo");
+	}
 }
